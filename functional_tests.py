e6ddc8e87f78f9abf2c5827b243217f706e431bc
Samuel Briley <sjbriley11@gmail.com>
2021-01-14 21:57:07 -0600

    installed modules into virt env, added functional_tests, added .gitignore to ignore sqlite, geckolog, virtualenv, and hopefully .pyc and cache files

---
+++ functional_tests.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import unittest
+
+#Make sure 'TestCase' is capitized
+class NewVisitorTest(unittest.TestCase):
+
+    #Make sure the 'Up' and the 'Down' are capitlized
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+    
+    #unittest automatically runs any function that starts with 'test'
+    #also always runs setUp first, then finishes with tearDown no matter what
+    def test_Start(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
