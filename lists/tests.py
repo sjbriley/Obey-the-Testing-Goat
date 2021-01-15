675ba7d195338ff96cbfa24ca959b10b9c80100b
Samuel Briley <sjbriley11@gmail.com>
2021-01-14 22:19:43 -0600

    Added unit test to check if '/' goes to home_page in lists.views and it succeeded

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,17 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
+#Run with 'python manage.py tests'
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+class HomePageTest(TestCase):
+
+    #This checks if resolve when called with '/' 
+    #finds a function called 'home_page' 
+    #home_page is the view function that we wrote AFTER trying the test
+    #so initially it fails, then eventually after code will pass
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
