48a3162b506f8f24e023980ba067e318c03d0ed2
Samuel Briley <sjbriley11@gmail.com>
2021-01-17 11:02:32 -0600

    Changed functional tests so that it checks for to-do items being added. Have no implemented code yet to do that

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 #Functional tests test the application from the outside
@@ -17,8 +19,35 @@ class NewVisitorTest(unittest.TestCase):
     #unittest automatically runs any function that starts with 'test'
     #also always runs setUp first, then finishes with tearDown no matter what
     def test_Start(self):
+        
+        #Edith heads to the homepage of a to-do page
         self.browser.get('http://localhost:8000')
+
+        #She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        #She is invited to enter a to-do immediately
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        
+        #she types 'Buy peacock feathers' into the text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        #she hits enter and the page updates listing
+        #'1: Buy peacock feathers' as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        #give page a second to load before we make new assertions
+        #called an 'explicit wait'
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
+        #there is still a text box inviting her to add another item
+        #she enters 'Use peacock feathers to make a fly"
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
