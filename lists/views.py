43bea91158bea644166d94b8445a00d1c3996a7c
Samuel Briley <sjbriley11@gmail.com>
2021-01-18 11:56:32 -0600

    Migrated the database and succesffully completed FT, just need a way to reset the inputs

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
+    #If we receiver a post, we redirect
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+        
+    items = Item.objects.all()
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+        'items': items
+    }) 
