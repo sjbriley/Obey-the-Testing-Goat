be5312c2722bf02be6454fd6280c8f5a9db1a674
Samuel Briley <sjbriley11@gmail.com>
2021-01-18 12:53:42 -0600

    added second test to test multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -37,7 +37,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     #unittest automatically runs any function that starts with 'test'
     #also always runs setUp first, then finishes with tearDown no matter what
-    def test_Start(self):
+    def test_can_start_a_list_for_one_user(self):
         
         #Edith heads to the homepage of a to-do page
         self.browser.get(self.live_server_url)
@@ -59,7 +59,6 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         #give page a second to load before we make new assertions
         #called an 'explicit wait'
-        time.sleep(1)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         #There is another page inviting her to make a new item
@@ -67,15 +66,56 @@ class NewVisitorTest(LiveServerTestCase):
         #she types a new to-do item in:
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         #The page updates and now shows her items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        #there is still a text box inviting her to add another item
-        #she enters 'Use peacock feathers to make a fly"
+        #Edith wonders if the site remembers her lists, and sees that there is a unique URL
         self.fail('Finish the test!')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        #she notices her list has a unique URL
+        edith_list_url = self.browser.current_url
+        #assertRegex is from unittest that checks if string matches regular expression
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        #a new user, francis, comes
+        #We use new browser session to reset Ediths cookies
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        #francis visits homepage, no sign of edith
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        #francis starts a new list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        #francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #again, there is no trace of edits list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+
+
 
 #Now that we use LiveServerTestCase and created a new folder with init file, we don't need this
 '''
