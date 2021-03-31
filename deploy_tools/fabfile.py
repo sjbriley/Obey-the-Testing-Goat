4b9c947bb61eb1181598d4a47406018a6a354657
Samuel Briley <sjbriley11@gmail.com>
2021-03-30 19:03:57 -0500

    fixed parenthesis

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -7,8 +7,8 @@ REPO_URL = 'https://github.com/sjbriley/to_do.git'
 def deploy():
     # env.user is signed in user, env.host is address of server from command line
     # env.host: /www.mydjangoproject.xyz/to_do/
-    site_folder = f'/home/{env.user}/sites/{env.host}'
-    run(f'mkdir -p {site_folder}')
+    site_folder = f"/home/{env.user}/sites/{env.host}"
+    run(f"mkdir -p {site_folder}")
     with cd(site_folder): # run these commands inside this directory
         _get_latest_source()
         _update_virtualenv()
@@ -17,28 +17,28 @@ def deploy():
         _update_database()
 
 def _get_latest_source():
-    if exists('.git'):
-        run('git fetch')
+    if exists(".git"):
+        run("git fetch")
     else:
-        run(f'git clone {REPO_URL}')
-    current_commit = local('git log -n 1 -- format=%H', capture=True)
-    run(f'git reset --hard {current_commit}')
+        run(f"git clone {REPO_URL}")
+    current_commit = local("git log -n 1 -- format=%H", capture=True)
+    run(f"git reset --hard {current_commit}")
 
 def _update_virtualenv():
-    if not exists('virtualenv/bin/pip'):
-        run(f'python3.7 -m venv virtualenv')
-    run('./virtualenv/bin/pip install -r requirements.txt')
+    if not exists("virtualenv/bin/pip"):
+        run(f"python3.7 -m venv virtualenv")
+    run("./virtualenv/bin/pip install -r requirements.txt")
 
 def _create_or_update_dotenv():
-    append('.env', 'DJANGO_DEBUG_FALSE=y')
-    append('.env', f'SITENAME={env.host}')
-    current_contents = run('cat .env')
-    if 'DJANGO_SECRET_KEY' not in current_contents:
-        new_secret = ''.join(random.SystemRandom().choices('abcdefghijklmnopqrestuvwxyz0123456789', k=50))
-        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+    append(".env", "DJANGO_DEBUG_FALSE=y")
+    append(".env", f"SITENAME={env.host}")
+    current_contents = run("cat .env")
+    if "DJANGO_SECRET_KEY" not in current_contents:
+        new_secret = "".join(random.SystemRandom().choices('abcdefghijklmnopqrestuvwxyz0123456789', k=50))
+        append(".env", f"DJANGO_SECRET_KEY={new_secret}")
 
 def _update_static_files():
-    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+    run("./virtualenv/bin/python manage.py collectstatic --noinput")
 
 def _update_database():
-    run('./virtualenv/bin/python manage.py migrate --noinput')
+    run("./virtualenv/bin/python manage.py migrate --noinput")
