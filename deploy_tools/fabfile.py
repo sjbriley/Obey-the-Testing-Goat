12abd4b1bd35862a9314e0ef23755a6c23244e7e
Samuel Briley <sjbriley11@gmail.com>
2021-06-06 19:39:46 -0500

    fixed hopefully fabfile.py

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -27,27 +27,27 @@ def _get_latest_source():
         run('git fetch')
     else:
         run(f'git clone {REPO_URL}')
-    current_commit = local("git log -n 1 -- format=%H", capture=True)
+    current_commit = local("git log -n 1 --format=%H", capture=True)
     run(f"git reset --hard {current_commit}")
 
 def _update_virtualenv():
     if not exists("virtualenv/bin/pip"):
-        run(f"python3.7 -m venv virtualenv")
+        run(f"python3 -m venv virtualenv")
     run("./virtualenv/bin/pip install -r requirements.txt")
 
 def _create_or_update_dotenv():
     append(".env", "DJANGO_DEBUG_FALSE=y")
-    append(".env", f"SITENAME={env.host}")
+    append(".env", f"SITENAME=.mydjangoproject.xyz")
     current_contents = run("cat .env")
     if "DJANGO_SECRET_KEY" not in current_contents:
         new_secret = "".join(random.SystemRandom().choices('abcdefghijklmnopqrestuvwxyz0123456789', k=50))
         append(".env", f"DJANGO_SECRET_KEY={new_secret}")
 
 def _update_static_files():
-    run("./virtualenv/bin/python manage.py collectstatic --noinput")
+    run("./virtualenv/bin/python3 manage.py collectstatic --noinput")
 
 def _update_database():
-    run("./virtualenv/bin/python manage.py migrate --noinput")
+    run("./virtualenv/bin/python3 manage.py migrate --noinput")
 
 if __name__=='__main__':
     deploy()
